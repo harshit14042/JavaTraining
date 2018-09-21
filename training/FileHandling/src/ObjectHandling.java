862f03a7ee8e62beaf3af8aebbe04ce058064e1f
AC31841 <AC31841@SGH526RPQL.ctl.intranet>
2018-09-21 13:19:09 +0530

    Third Commit

--- training/FileHandling/src/ObjectHandling.java
+++ training/FileHandling/src/ObjectHandling.java
@@ -1,5 +1,16 @@
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ObjectHandling {
 	
@@ -43,4 +54,53 @@ public class ObjectHandling {
 		}
 		return obj;
 	}
+	
+	public boolean writeToTextFile(Customer cust){
+		boolean status=false;
+		
+		PrintWriter writer =null;
+		
+		try {
+			writer=new PrintWriter(new FileWriter(new File("Customer.txt")));
+			writer.write(cust.toString());
+			status=true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}finally{
+			writer.close();
+		}
+		
+		return status;
+	}
+	
+	public List<Customer> readFromTextFile(File file){
+		List<Customer> custList=new ArrayList<Customer>();
+		
+		BufferedReader reader=null;
+		
+		try {
+			reader=new BufferedReader(new FileReader(file));
+			String custAsString;
+			while((custAsString=reader.readLine())!=null){
+				String[] items=custAsString.split(",");
+				Customer cust=new Customer(Long.parseLong(items[0]),items[1],Long.parseLong(items[2]));
+				custList.add(cust);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				reader.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		return custList;
+	}
+	
+	
 }
