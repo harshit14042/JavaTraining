614e2e1e50a21f178fc36c9007996c07daae4973
harshit14042 <harshit14042@iiitd.ac.in>
2018-09-29 16:06:00 +0530

    testing laptop git

--- WebApplication/src/com/training/LoginServlet.java
+++ WebApplication/src/com/training/LoginServlet.java
@@ -29,6 +29,7 @@ public class LoginServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		doPost(request, response);
+		System.out.println();
 	}
 
 	/**
