e6e90aadc76bf9c6188ab366bf1d7d634a1192ce
AC31841 <AC31841@SGH526RPQL.ctl.intranet>
2018-10-03 13:57:39 +0530

    *

---
+++ WebApplication/src/com/training/LifeCycle.java
@@ -0,0 +1,47 @@
+package com.training;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LifeCycle
+ */
+public class LifeCycle extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LifeCycle() {
+        super();
+        System.out.println("Constructor called");
+    }
+
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		//super.init();
+		System.out.println("Init Method called");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("doGet called").append(request.getContextPath());
+		System.out.println("doGet");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
