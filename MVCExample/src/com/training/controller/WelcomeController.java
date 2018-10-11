abe89dc94bd8b162f771f450209cd976a10130ba
AC31841 <AC31841@SGH526RPQL.ctl.intranet>
2018-10-11 17:29:36 +0530

    **

---
+++ MVCExample/src/com/training/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.training.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class WelcomeController {
+
+	@Autowired
+	ModelAndView mdlView;
+	
+	public ModelAndView init() {
+		mdlView.add
+	}
+	
+}
